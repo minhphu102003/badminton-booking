9a7df7315e9763e992f25988e0390ada0b6cb754
Tobi <124958881+minhphu102003@users.noreply.github.com>
2025-07-27 18:32:58 +0700

    feat: create layout for private routes (#17)

---
+++ src/app/presentation/icons/LogoutICon.tsx
@@ -0,0 +1,5 @@
+import BaseIcon from './BaseIcon';
+
+export default function LogoutIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
+  return <BaseIcon iconClass="bi bi-box-arrow-right" {...props} />;
+}
